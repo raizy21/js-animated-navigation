3391c1461ca0be8b626e3d3d26752767b8c3e49e
raizy21 <chiper_raizy@yahoo.com>
2021-01-04 21:49:16 +0100

    animate_navigation_code

---
+++ script.js
@@ -0,0 +1,53 @@
+/**************************************
+ *
+ * Links
+ * {@link} - https://developer.mozilla.org/en-US/docs/Web/CSS/Using_CSS_custom_properties
+ * {@link} - https://paletton.com/#uid=1000u0kllllaFw0g0qFqFg0w0aF
+ * {@link} - https://www.w3schools.com/howto/howto_css_menu_icon.asp
+ * {@link} - https://developer.mozilla.org/en-US/docs/Web/CSS/animation
+ *****************************************/
+
+const menuBars = document.getElementById('menu-bars');
+const overlay = document.getElementById('overlay');
+const nav1 = document.getElementById('nav-1');
+const nav2 = document.getElementById('nav-2');
+const nav3 = document.getElementById('nav-3');
+const nav4 = document.getElementById('nav-4');
+const nav5 = document.getElementById('nav-5');
+const navItems = [nav1, nav2, nav3, nav4, nav5];
+
+
+//control navigation animation 
+function navAnimation(direction1, direction2) {
+    navItems.forEach((nav, i) => {
+        nav.classList.replace(`slide-${direction1}-${i + 1}`, `slide-${direction2}-${i + 1}`);
+    });
+}
+
+function toggleNav() {
+    //toggle: menu bars open/closed
+    menuBars.classList.toggle('change');
+    //toggle: menu active
+    overlay.classList.toggle('overlay-active');
+    if (overlay.classList.contains('overlay-active')) {
+
+        //animate in- overlay
+        overlay.classList.replace('overlay-slide-left', 'overlay-slide-right');
+
+        //animate in - nav items
+        navAnimation('out', 'in');
+
+    } else {
+        //animate out- overlay
+        overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');
+
+        //animate out - nav items
+        navAnimation('in', 'out');
+    }
+}
+
+//event listeners
+menuBars.addEventListener('click', toggleNav);
+navItems.forEach((nav) => {
+    nav.addEventListener('click', toggleNav);
+});
